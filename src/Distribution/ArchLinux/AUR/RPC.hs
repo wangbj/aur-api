b1f2e97010c74b6ac6f3abec9dfc263ab6dc4d04
Baojun Wang <wangbj@gmail.com>
2016-06-15 15:34:30 -0700

    catch exception in AUR RPC query into ExceptT

--- src/Distribution/ArchLinux/AUR/RPC.hs
+++ src/Distribution/ArchLinux/AUR/RPC.hs
@@ -24,19 +24,19 @@ genURL (QSearch Nothing txt) = aurServer ++ "&type=search&arg=" ++ txt
 genURL (QSearch (Just field) txt) = aurServer ++ "&type=search&by=" ++ show field ++ "&arg=" ++ txt
 genURL (QInfo q) = aurServer ++ "&type=info" ++ concatMap (\x -> "&arg%5b%5d="++x) q
 
-queryAUR :: (MonadIO m, MonadCatch m) => AURQuery -> ExceptT String m [AURInfo]
-queryAUR s = do
-  r <- liftIO $ get (genURL s) `catchAll` (\(SomeException e) -> fail (show e))
-  let res = eitherDecode (r ^. responseBody) >>= getAURInfo
-  ExceptT (return $! res)
+getM :: (MonadIO m) => AURQuery -> ExceptT String m (Response LBS.ByteString)
+getM s = ExceptT . liftIO $ fmap Right (get (genURL s)) `catch` (\(SomeException e) -> return (Left (show e)))
 
-info :: (MonadIO m, MonadCatch m) => [String] -> ExceptT String m [AURInfo]
+queryAUR :: (MonadIO m) => AURQuery -> ExceptT String m [AURInfo]
+queryAUR s = getM s >>= \r -> ExceptT . return $ (eitherDecode >=> getAURInfo) (r ^. responseBody)
+
+info :: (MonadIO m) => [String] -> ExceptT String m [AURInfo]
 info = queryAUR . QInfo
 
-search :: (MonadIO m, MonadCatch m) => String -> ExceptT String m [AURInfo]
+search :: (MonadIO m) => String -> ExceptT String m [AURInfo]
 search = queryAUR . QSearch Nothing
 
-searchBy :: (MonadIO m, MonadCatch m) => SearchBy -> String -> ExceptT String m [AURInfo]
+searchBy :: (MonadIO m) => SearchBy -> String -> ExceptT String m [AURInfo]
 searchBy f = queryAUR . QSearch (Just f)
 
 url1 = "https://aur.archlinux.org/rpc/?v=5&type=info&arg%5b%5d=icaclient"
